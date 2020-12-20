bafcba2f5b20ca80fcbf5fdd8cf4e6005551640b
Coder747 <ayrefaie@live.com>
2020-12-20 13:03:18 +0200

    dictionary, but button is broken

---
+++ ibrahim.py
@@ -0,0 +1,30 @@
+﻿import os
+import tkinter as tk
+
+
+def getdir():
+    dict = {}
+    for root, dirs, files in os.walk(os.getcwd()+"\\Test"):
+        dict[root] = files
+    return dict
+
+def open_pdf(filepath,filename):
+    filepath += f"\\{filename}"
+    os.startfile(filepath)
+
+myfiles = getdir()
+
+########################################################
+#GUI
+########################################################
+
+root=tk.Tk()
+root.geometry("350x250+700+400")
+
+for filepath,filelist in myfiles.items():
+    for filename in filelist:
+        n_Button= tk.Button(root,text=filename,command = lambda: open_pdf(filepath,filename),font=('arial',15,'bold'))
+        n_Button.pack()
+
+
+root.mainloop()
